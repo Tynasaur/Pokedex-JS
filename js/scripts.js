bef6156d8484f241e48893a39a15edc3625d7927
Tynasaur <70995431+Tynasaur@users.noreply.github.com>
2021-02-23 18:31:59 -0600

    added button inside li to html file

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,7 @@ function addListItem(pokemon) {
   let listpokemon = document.createElement("li");
   let button = document.createElement("button");
   button.innerText = pokemon.name;
-  button.classList.add("button-class");
+  button.classList.add("button-class"); //for css
   listpokemon.appendChild(button);
   pokemonList.appendChild(listpokemon);
   button.addEventListener("click", function(event) {
